cb28a3ee1b283f8fdf7fa7f0840da8ab43b610d0
Felyppe Nunes <121322408+Felyppe1@users.noreply.github.com>
2024-08-20 18:37:18 +0000

    feat: movie theater schedules

--- domain/movie-theater/movie-theater.ts
+++ domain/movie-theater/movie-theater.ts
@@ -8,7 +8,8 @@ type MovieTheaterProps = {
     name: string
     address: Address
     chairCatalog: ChairType[]
-    rooms: Room[]
+    rooms: Room[],
+    schedules: Movie[]
 }
 
 type CreateMovieTheaterProps = {
@@ -28,6 +29,7 @@ export class MovieTheater {
     address: Address
     chairCatalog: ChairType[]
     rooms: Room[]
+    schedules: Movie[]
     
     static create({
         name,
@@ -38,7 +40,8 @@ export class MovieTheater {
             name,
             address,
             chairCatalog: [],
-            rooms: []
+            rooms: [],
+            schedules: []
         })
     }
 
@@ -47,7 +50,8 @@ export class MovieTheater {
         name,
         address,
         chairCatalog,
-        rooms
+        rooms,
+        schedules
     }: MovieTheaterProps) {
         if (!id) {
             throw new Error('Id is required')
@@ -69,12 +73,19 @@ export class MovieTheater {
             throw new Error('Rooms are required')
         }
 
+        if (!schedules) {
+            throw new Error('Schedules are required')
+        }
+
         this.id = id
         this.name = name
         this.address = address
         this.chairCatalog = []
-        this.rooms = rooms
-        chairCatalog.forEach(chairType => this.addChairType(chairType))
+        this.rooms = []
+        this.schedules = []
+        chairCatalog.forEach(newChairType => this.addChairType(newChairType))
+        rooms.forEach(newRoom => this.addRoom(newRoom))
+        schedules.forEach(newMovie => this.addMovie(newMovie))
     }
 
     findChairTypeById(chairTypeId: string) {
@@ -166,5 +177,90 @@ export class MovieTheater {
 
     removeRoomById(roomId: string) {
         this.rooms = this.rooms.filter(room => roomId !== room.getId())
+    }
+
+    findMovieById(movieId: string) {
+        return this.schedules.find(movie => movieId === movie.getId())
+    }
+
+    addMovie(newMovie: Movie) {
+        const movieFoundById = this.findMovieById(newMovie.getId())
+
+        if (movieFoundById) {
+            throw new Error(`Movie id ${newMovie.getId()} already exists`)
+        }
+
+        this.schedules.push(newMovie)
+    }
+}
+
+
+type MovieProps = {
+    movieId: string
+    exhibitionPeriod: ExhibitionPeriod
+}
+
+export class Movie {
+    private movieId: string
+    private exhibitionPeriod: ExhibitionPeriod
+
+    constructor({
+        movieId,
+        exhibitionPeriod
+    }: MovieProps) {
+        if (!movieId) {
+            throw Error('Movie id is required')
+        }
+
+        if (!exhibitionPeriod) {
+            throw Error('Exhibition period is required')
+        }
+
+        this.movieId = movieId
+        this.exhibitionPeriod = exhibitionPeriod
+    }
+
+    
+    getId() {
+        return this.movieId
+    }
+}
+
+
+type ExhibitionPeriodProps = {
+    start: Date
+    end: Date
+}
+
+export class ExhibitionPeriod {
+    private start: Date
+    private end: Date
+
+    constructor({
+        start,
+        end
+    }: ExhibitionPeriodProps) {
+        if (!start) {
+            throw new Error('Start is required')
+        }
+
+        if (!end) {
+            throw new Error('End is required')
+        }
+
+        if (end <= start) {
+            throw new Error('End must be greater than the start date')
+        }
+
+        this.start = start
+        this.end = end
+    }
+
+    getStart() {
+        return this.start
+    }
+
+    getEnd() {
+        return this.end
     }
 }
